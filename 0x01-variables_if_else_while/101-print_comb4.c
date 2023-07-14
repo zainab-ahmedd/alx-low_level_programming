5ef47ac785575790088ad4dcfa24bad212378cc6
Zainab <zainab.elhady@gmail.com>
2023-07-14 16:40:34 -0700

    ADVANCED

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * main - Print different combinations of three digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2, n3;
+
+	for (n1 = 48; n1 <= 55; n1++)
+	{
+		for (n2 = 49; n2 <= 56; n2++)
+		{
+			for (n3 = 50; n3 <= 57; n3++)
+			{
+				if (n3 > n2 && n2 > n1)
+				{
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
+					if (n1 != 55 || n2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
