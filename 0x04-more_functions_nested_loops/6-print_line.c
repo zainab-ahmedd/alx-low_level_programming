fb3ca8de8249510fa66a1d0cda846b9dc6cb2512
Zainab <zainab.elhady@gmail.com>
2023-07-20 19:29:19 -0700

    draw line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n >= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i <= n; i++)
+		{
 			_putchar('-');
+		}
 			_putchar('\n');
 	}
 }
