91ade68851a549de51f62611c8c91be3b8bd1c43
Zainab <zainab.elhady@gmail.com>
2023-07-20 18:06:07 -0700

    print num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,16 @@ void more_numbers(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-	       for (n2 = 0; n2 <= 14; n2++)
-	       {
-		       n3 = n2;
-		       if (n2 >= 10)
-		       {
-			       _putchar(1 + 48);
-			       n3 = n2 % 10;
-		       }
-		       _putchar(n3 + 48);
-	       }
-	       _putchar('\n');
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+			n3 = n2;
+			if (n2 >= 10)
+			{
+				_putchar(1 + 48);
+				n3 = n2 % 10;
+			}
+			_putchar(n3 + 48);
+		}
+		_putchar('\n');
 	}
 }
