723b101d7d1e7e999ca2c3682a3920ab2dd336fa
Zainab <zainab.elhady@gmail.com>
2023-07-20 19:25:42 -0700

    draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - Entry point
+ *
+ * print_line - draws a straight line in the terminal
+ * @n:  is the number of times the character _ should be printed
+ * Return: 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n >= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('-');
+			_putchar('\n');
+	}
+}
