95ee5c4001473c2a1b2062e68fc56f9b063f006b
Zainab <zainab.elhady@gmail.com>
2023-07-24 17:47:02 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ *
+ * reset_to_98 - updates the value of pointer
+ * @n: pointer
+ * Return: 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Araay&pointer
