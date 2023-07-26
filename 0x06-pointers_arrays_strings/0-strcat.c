a138d97c3bb1c7eafe110e2f7a1a1894d8039312
Zainab <zainab.elhady@gmail.com>
2023-07-26 15:48:04 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,12 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j;
 
-	for (; dest[i] != '0'; i++)
+	for (; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; src[j] != 0; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		dest++;
+		dest[i] = src[j];	
 		i++;
 	}
 	dest[i] = '\0';
