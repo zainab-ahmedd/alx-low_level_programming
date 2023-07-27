051ede47671c5be3234a4c15a65b3a470be3205a
Zainab <zainab.elhady@gmail.com>
2023-07-27 15:33:36 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *string_toupper - Entry point
+ *
+ * *string_toupper - changes all lowercase letters of a string to uppercase
+ * Return: 0 (Success)
+*/
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] <= 122 && c[i] >= 97)
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
