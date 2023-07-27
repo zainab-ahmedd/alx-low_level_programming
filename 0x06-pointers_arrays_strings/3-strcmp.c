e6296074155ce2343625bb51c4fcb5e2df77a0bb
Zainab <zainab.elhady@gmail.com>
2023-07-26 19:49:17 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, comp;
+	int i, j, comp;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	j = 0;
+	for (i = 0; s1[i] != '\0' && s2[j] != '\0'; i++)
 	{
+		comp = s1[i] - s2[j];
+		j++;
 	}
-	comp = s1[i] - s2[i];
 	return (comp);
 }
