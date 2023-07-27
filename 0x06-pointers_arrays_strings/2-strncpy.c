57ff6a7fa877f698506d9ac1e0cccce43baac025
Zainab <zainab.elhady@gmail.com>
2023-07-26 17:44:58 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strncpy - Entry point
+ *
+ * *_strncpy - copy a string
+ * @dest: to string
+ * @src: from string
+ * @n: bytes
+ * Return: 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0, src[i] != ('\0') && i < n; i++)
+	{
+		dest[i] = scr[i];
+	}
+	for (i = 0 ;i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
