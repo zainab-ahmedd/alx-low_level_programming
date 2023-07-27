b65107a1538d36aaa5587c1eb5280b2105fba088
Zainab <zainab.elhady@gmail.com>
2023-07-27 16:33:41 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *leet - Entry point
+ *
+ * *leet - encodes a string
+ * @c: string to be leet
+ * Return: 0 (Success)
+*/
+char *leet(char *c)
+{
+	int i, j;
+
+	char string[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == string[j])
+			{
+				c[i] == num[j];
+			}
+		}
+	}
+	return (c);
+}
