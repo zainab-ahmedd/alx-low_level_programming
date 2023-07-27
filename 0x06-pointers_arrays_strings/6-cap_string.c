b3f36c3fb2841a73a8cea158b1632c2fc95be145
Zainab <zainab.elhady@gmail.com>
2023-07-27 16:07:56 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *cap_string - Entry point
+ *
+ * *cap_string - capitalizes all words of a string.
+ * @c: string to be cap
+ * Return: 0 (Success)
+*/
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] <= 122 && c[i] >= 97)
+		{
+			if (c[i] == c[0] || c[i - 1] == ' ' || c[i - 1] == '\t'
+			|| c[i - 1] == '(' || c[i - 1] == ')' || c[i - 1] == '{'
+			|| c[i - 1] == '}' || c[i - 1] == '\n' || c[i - 1] == '"'
+			|| c[i - 1] == ',' || c[i - 1] == ';' || c[i - 1] == '.'
+			|| c[i - 1] == '!' || c[i - 1] == '?')
+			{
+				c[i] = c[i] - 32;
+			}
+		}
+	}
+	return (c)
+}
