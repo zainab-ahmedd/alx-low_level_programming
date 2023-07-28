82b04bb083d9836bd9a88f304680c3b9a21572df
Zainab <zainab.elhady@gmail.com>
2023-07-27 17:51:29 -0700

    int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - Entry point
+ *
+ * print_number - prints an integer
+ * @n: integer
+ * Return: 0 (Success)
+*/
+void print_number(int n)
+{
+	unsigned int m;
+
+	m = n;
+	if (m < 0)
+	{
+		_putchar("-");
+		m = -n;
+	}
+	if (m / 10 != 0)
+	{
+		_putchar(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
