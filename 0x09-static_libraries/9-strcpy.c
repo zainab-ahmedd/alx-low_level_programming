517e25580cf778372d2d5603024e1560bc32d13e
Zainab <zainab.elhady@gmail.com>
2023-08-07 13:34:28 -0700

    lib.

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,20 +1,25 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * *_strcpy - Entry point
- *
- * *_strcpy - string reverse
+ * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
- * Return: 0 (Success)
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int a = 0;
+	int l = 0;
+	int x = 0;
 
-	for ( ; a > l ; a++)
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
 	{
 		dest[x] = src[x];
 	}
 	dest[l] = '\0';
 	return (dest);
+}
+
